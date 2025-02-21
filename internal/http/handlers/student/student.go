953ac90d5e7ca0974df517a443ac53624f7fa2c1
madihanazir <madihan541@gmail.com>
2025-02-21 23:18:02 +0530

    add request validation

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -1,12 +1,48 @@
 package student
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/madihanazir/students-api/internal/types"
+	"github.com/madihanazir/students-api/internal/utils/response"
 )
 
 // New returns an HTTP handler function for the students API
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("creating new student")
+		var student types.Student
+
+		err := json.NewDecoder(r.Body).Decode(&student)
+		if errors.Is(err, io.EOF) {
+			response.WriteJSON(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("empty body")))
+			return
+		}
+		if err != nil {
+			response.WriteJSON(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+		validate := validator.New()
+		if err := validate.Struct(student); err != nil {
+			validateErrs, ok := err.(validator.ValidationErrors)
+
+			if ok {
+				response.WriteJSON(w, http.StatusBadRequest, response.ValidationError(validateErrs))
+			} else {
+				response.WriteJSON(w, http.StatusBadRequest, response.GeneralError(err))
+			}
+			return
+		}
+
+		response.WriteJSON(w, http.StatusCreated, map[string]string{"success": "OK"})
+		//request validation
+
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Students API is working"))
